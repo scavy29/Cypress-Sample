f27dd169a0c9ad56175c030ac960b0a96ca982bc
Weeraporn Paisingkhon <weeraporn.pai@ascendcorp.com>
2021-05-13 18:01:51 +0700

    update code to use await

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -6,39 +6,33 @@ const oracledb = require("oracledb");
  * @type {Cypress.PluginConfig}
  */
 
-// oracledb.initOracleClient({ libDir: "/Users/weerapornpaisingkhon/instantclient_19_3" }); //update to your path and delete the comment
+oracledb.initOracleClient({ libDir: "/Users/weerapornpaisingkhon/instantclient_19_3" }); //update to your path and delete the comment
 
-const queryData = (query, dbconfig) => {
-  return new Promise((resolve, reject) => {
-    oracledb.getConnection(dbconfig, (err, connection) => {
-      console.log("Starting a connection. . . . . ");
 
-      if (err) {
-        console.error(err.message);
-        return;
+const queryData = async(query, dbconfig) => {
+  let conn;
+  try{
+      conn = await oracledb.getConnection(dbconfig);
+      return result = await conn.execute(query);
+  }catch(err){
+      console.log("Error===>"+err)
+      return err
+  } finally{
+    if(conn){
+      try{
+        conn.close();
+      }catch(err){
+        console("Error===>"+err);
       }
-
-      console.log("Connection was successful!!!");
-
-      connection.execute(query, (err, result) => {
-        if (err) {
-          error = err;
-          console.log("SQL error ====>" + error);
-          reject(error);
-        } else {
-          resolve(result);
-          connection.close();
-        }
-      });
-    });
-  });
-};
+    }
+  }
+}
 
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
   on("task", {
-    sqlQuery: (query) => {
+    sqlQuery: query => {
       return queryData(query, config.env.db);
     },
   });
